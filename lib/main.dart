bd1569cdd41daf6c4332168feea6de30891834ff
JumpeiMatsuo <beatsjunpei@gmail.com>
2022-04-28 19:25:08 +0900

    タイルとかを試した。

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.orange),
-      home: SplashWidget(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SplashWidget(),
     );
   }
 }
 
 class SplashWidget extends StatelessWidget {
+  const SplashWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -41,6 +43,8 @@ class SplashWidget extends StatelessWidget {
 }
 
 class TabContainerWidget extends StatefulWidget {
+  const TabContainerWidget({Key? key}) : super(key: key);
+
   @override
   _TabContainerWidget createState() => _TabContainerWidget();
 }
@@ -52,17 +56,51 @@ class _TabContainerWidget extends State<TabContainerWidget> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("タブコンテイナー"),
-        ),
-        body: tabPageList[selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.message), label: "Diary"),
-            BottomNavigationBarItem(icon: Icon(Icons.book), label: "Todo")
-          ],
-          onTap: (index) => {_onTapItem(index)},
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("タブコンテイナー"),
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: const [
+                Tab(
+                    child: Text(
+                  "日記",
+                  style: TextStyle(color: Colors.white),
+                  maxLines: 1,
+                )),
+                Tab(
+                    child: Text(
+                  "TODO",
+                  style: TextStyle(color: Colors.white),
+                ))
+              ],
+              onTap: (index) => {_onTapItem(index)},
+            ),
+          ),
+          body: TabBarView(children: tabPageList),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: selectedIndex,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.message), label: "Diary"),
+              BottomNavigationBarItem(icon: Icon(Icons.book), label: "Todo")
+            ],
+            onTap: (index) => {_onTapItem(index)},
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.book),
+            onPressed: () => {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (build) {
+                    return Container(
+                      color: Colors.blue,
+                    );
+                  })
+            },
+          ),
         ));
   }
 
@@ -74,11 +112,58 @@ class _TabContainerWidget extends State<TabContainerWidget> {
 }
 
 class DiaryPage extends StatelessWidget {
+  DiaryPage({Key? key}) : super(key: key);
+
+  Card _createCard(Diary diary, BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(diary.title),
+        subtitle: Text(diary.subTitle),
+        leading: const CircleAvatar(
+          child: Icon(Icons.book),
+        ),
+        trailing: Icon(Icons.more_vert),
+        onTap: () async {
+          var editDiary = await Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return DiaryDetail(
+              diary: diary,
+            );
+          }));
+        },
+      ),
+    );
+  }
+
+  List<Diary>? diaryList = [
+    Diary("松尾", "楽しかった", "あ"),
+    Diary("松尾", "悲しかった", "あ"),
+    Diary("松尾", "怒った", "あ"),
+    Diary("松尾", "愛した", "あ")
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Center(
-      child: Text("Diary"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          "直近の日記",
+          style: TextStyle(color: Color.fromARGB(200, 0, 0, 0), fontSize: 20),
+        ),
+        Container(
+          height: 400,
+          child: ListView.builder(
+              itemCount: diaryList?.length ?? 0,
+              itemBuilder: (context, index) {
+                return _createCard(diaryList![index], context);
+              }),
+        ),
+        const Text(
+          "1年前の日記",
+          style: TextStyle(color: Color.fromARGB(200, 0, 0, 0), fontSize: 20),
+        )
+      ],
     );
   }
 }
@@ -89,6 +174,42 @@ class TodoPage extends StatelessWidget {
     // TODO: implement build
     return const Center(
       child: Text("Todo"),
+    );
+  }
+}
+
+class CreateDiary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
+
+class Diary {
+  Diary(this.name, this.title, this.subTitle);
+
+  String name = "デフォルト";
+  String title = "タイトル";
+  String subTitle = "サブタイトル";
+  DateTime dateTime = DateTime.now();
+}
+
+class DiaryDetail extends StatelessWidget {
+  final Diary diary;
+
+  DiaryDetail({required this.diary}) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [Text(diary.name), Text(diary.title), Text(diary.subTitle)],
+      ),
     );
   }
 }
